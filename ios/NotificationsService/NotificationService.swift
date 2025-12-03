c75572698d78dfe0091aed0ee2f89a02195bc215
DanilaSakSyn <danilasak@syndicate-g.com>
2025-12-03 13:46:09 +0300

    hot fixes

--- ios/NotificationsService/NotificationService.swift
+++ ios/NotificationsService/NotificationService.swift
@@ -1,35 +1,43 @@
-//
-//  NotificationService.swift
-//  NotificationsService
-//
-//  Created by Данила Сак on 2.12.25.
-//
-
 import UserNotifications
+
+import FirebaseMessaging
 
 class NotificationService: UNNotificationServiceExtension {
 
-    var contentHandler: ((UNNotificationContent) -> Void)?
-    var bestAttemptContent: UNMutableNotificationContent?
+var contentHandler: ((UNNotificationContent) -> Void)?
 
-    override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
-        self.contentHandler = contentHandler
-        bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
-        
-        if let bestAttemptContent = bestAttemptContent {
-            // Modify the notification content here...
-            bestAttemptContent.title = "\(bestAttemptContent.title) [modified]"
-            
-            contentHandler(bestAttemptContent)
-        }
-    }
-    
-    override func serviceExtensionTimeWillExpire() {
-        // Called just before the extension will be terminated by the system.
-        // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
-        if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
-            contentHandler(bestAttemptContent)
-        }
-    }
+var bestAttemptContent: UNMutableNotificationContent?
+
+override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+
+self.contentHandler = contentHandler
+
+bestAttemptContent = request.content
+
+.mutableCopy() as? UNMutableNotificationContent
+
+guard let bestAttemptContent = bestAttemptContent else { return }
+
+FIRMessagingExtensionHelper().populateNotificationContent(
+
+bestAttemptContent,
+
+withContentHandler: contentHandler)
+
+}
+
+override func serviceExtensionTimeWillExpire() {
+
+// Called just before the extension will be terminated by the system.
+
+// Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
+
+if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
+
+contentHandler(bestAttemptContent)
+
+}
+
+}
 
 }
